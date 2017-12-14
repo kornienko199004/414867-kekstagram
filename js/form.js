6a0015de097deb4b619e6424c77710b98b230599
kornienko199004 <kornienko19904@gmail.com>
2017-12-15 01:36:35 +0300

    Добавил два новых модуля

--- js/form.js
+++ js/form.js
@@ -2,30 +2,19 @@
 
 (function () {
   var CODE_ESC = 27;
-  var SCALE_STEP = 25;
-  var MIN_SCALE = 25;
-  var MAX_SCALE = 100;
   var MAX_COMMENT_LENGTH = 140;
   var MAX_HASHTAG_LENGTH = 20;
-  var EFFECT_CLASS_NAME_PREFIX = 'upload-';
   var MAX_QUANTITY_OF_HASHTAGS = 5;
   var OVERLAY_HIDDEN_CLASS = 'hidden';
-  var DEFAULT_FILTER_VALUE = 100;
-
-  var setScale = function (scale) {
-    effectImagePreview.style.transform = 'scale(' + scale / 100 + ')';
-    uploadResizeControlsValue.value = scale + '%';
-  };
+  var DEFAULT_FILTER_EFFECT = 'none';
 
   var resetValues = function () {
-    scale = defaultScale;
-    setScale(scale);
     effectImagePreview.className = defaultEffectClassName;
     effect.checked = defaultEffect;
     uploadFormHashtagsElement.value = '';
     uploadFormDescriptionElement.value = '';
     resetSliderPosition();
-    setFilter('none');
+    setFilterEffect(DEFAULT_FILTER_EFFECT);
   };
 
   var resetSliderPosition = function () {
@@ -43,6 +32,7 @@
     if (uploadOverlay.classList.contains(OVERLAY_HIDDEN_CLASS)) {
       uploadOverlay.classList.remove(OVERLAY_HIDDEN_CLASS);
       resetValues();
+
     }
   };
 
@@ -60,75 +50,6 @@
       uploadFileElement.value = '';
     }
     hideSlider();
-  };
-
-  var aproximation = function (maxValueOfNewRange, maxValueOfProcess, value) {
-    var coefficient = maxValueOfNewRange / maxValueOfProcess;
-    var output = value * coefficient;
-    return output > maxValueOfNewRange ? maxValueOfNewRange : output;
-  };
-
-  var createFilterEffect = function (effectName, value) {
-    var maxValueOfProcess = 100;
-    switch (effectName) {
-      case 'effect-chrome':
-        return 'grayscale(' + aproximation(1, maxValueOfProcess, value) + ')';
-      case 'effect-sepia':
-        return 'sepia(' + aproximation(1, maxValueOfProcess, value) + ')';
-      case 'effect-marvin':
-        return 'invert(' + aproximation(100, maxValueOfProcess, value) + '%)';
-      case 'effect-phobos':
-        return 'blur(' + aproximation(3, maxValueOfProcess, value) + 'px)';
-      case 'effect-heat':
-        return 'brightness(' + aproximation(3, maxValueOfProcess, value) + ')';
-      default:
-        break;
-    }
-    return 'none';
-  };
-
-  var setFilter = function (filter) {
-    effectImagePreview.style.filter = filter;
-  };
-
-  var onRadioControlEffectChange = function (e) {
-    var str = EFFECT_CLASS_NAME_PREFIX;
-    var effectName = e.target.id.slice(str.length);
-
-    if (lastEffectName && effectImagePreview.classList.contains(lastEffectName)) {
-      effectImagePreview.classList.remove(lastEffectName);
-    }
-
-    effectImagePreview.classList.add(effectName);
-
-    if (effectName !== 'effect-none') {
-      if (uploadEffectLevelElement.classList.contains(OVERLAY_HIDDEN_CLASS)) {
-        uploadEffectLevelElement.classList.remove(OVERLAY_HIDDEN_CLASS);
-      }
-      setFilter(createFilterEffect(effectName, DEFAULT_FILTER_VALUE));
-      resetSliderPosition();
-    } else {
-      hideSlider();
-      setFilter('none');
-    }
-
-    lastEffectName = effectName;
-  };
-
-  var onResizeControlsButtonDecClick = function () {
-    scale = +uploadResizeControlsValue.value.slice(0, uploadResizeControlsValue.value.length - 1) - SCALE_STEP;
-    if (scale < MIN_SCALE) {
-      scale = MIN_SCALE;
-    }
-    setScale(scale);
-  };
-
-  var onResizeControlsButtonInkClick = function () {
-    scale = +uploadResizeControlsValue.value.slice(0, uploadResizeControlsValue.value.length - 1) + SCALE_STEP;
-    if (scale > MAX_SCALE) {
-      scale = MAX_SCALE;
-    }
-    setScale(scale);
   };
 
   var highlightElement = function (element, color) {
@@ -220,8 +141,7 @@
       uploadEffectLevelPinElement.style.left = left + 'px';
       uploadEffectLevelValElement.style.width = left + 'px';
       uploadEffectLevelValue.value = Math.round((left / maxWidthOfSlider) * 100);
-
-      setFilter(createFilterEffect(lastEffectName, uploadEffectLevelValue.value));
+      setFilterEffect(window.initializeFilters.createFilterEffect(uploadEffectLevelValue.value));
 
     };
     var onMouseUp = function (upEvt) {
@@ -233,23 +153,48 @@
     document.addEventListener('mouseup', onMouseUp);
   };
 
+  var setScale = function (scale) {
+    effectImagePreview.style.transform = 'scale(' + scale / 100 + ')';
+  };
+  var setFilterEffect = function (filter) {
+    effectImagePreview.style.filter = filter;
+  };
+  var setFilter = function (lastEffectName, filter, effectName) {
+    if (lastEffectName && effectImagePreview.classList.contains(lastEffectName) && effectName) {
+      effectImagePreview.classList.remove(lastEffectName);
+    }
+    if (effectName) {
+      effectImagePreview.classList.add(effectName);
+    }
+    effectImagePreview.style.filter = filter;
+    resetSliderPosition();
+    if (effectName !== 'effect-none') {
+      if (uploadEffectLevelElement.classList.contains(OVERLAY_HIDDEN_CLASS)) {
+        uploadEffectLevelElement.classList.remove(OVERLAY_HIDDEN_CLASS);
+      }
+    } else {
+      uploadEffectLevelElement.classList.add(OVERLAY_HIDDEN_CLASS);
+      setFilterEffect('none');
+    }
+    setFilterEffect(filter);
+  };
+
+
   var formElement = document.querySelector('.upload-form');
   var uploadFileElement = formElement.querySelector('#upload-file');
   var uploadOverlay = formElement.querySelector('.upload-overlay');
   var uploadFormCancelElement = formElement.querySelector('.upload-form-cancel');
   var uploadFormDescriptionElement = formElement.querySelector('.upload-form-description');
-  var uploadResizeControlsValue = formElement.querySelector('.upload-resize-controls-value');
+
   var uploadEffectControlElement = formElement.querySelector('.upload-effect-controls');
   var effectImagePreview = formElement.querySelector('.effect-image-preview');
   var uploadFormHashtagsElement = formElement.querySelector('.upload-form-hashtags');
-  var uploadResizeControlsButtonDec = formElement.querySelector('.upload-resize-controls-button-dec');
-  var uploadResizeControlsButtonInc = formElement.querySelector('.upload-resize-controls-button-inc');
-  var scale;
+  var scaleElement = document.querySelector('.upload-resize-controls');
+
   var effect = formElement.querySelector('[name=effect]');
   var defaultEffect = effect.checked;
-  var defaultScale = +uploadResizeControlsValue.value.slice(0, uploadResizeControlsValue.value.length - 1);
   var defaultEffectClassName = effectImagePreview.className;
-  var lastEffectName;
+
   var isCommentInputOnFocus;
 
   var uploadEffectLevelPinElement = formElement.querySelector('.upload-effect-level-pin');
@@ -261,19 +206,20 @@
   var defaultWidth = uploadEffectLevelValElement.style.width;
   var defaultLeft = uploadEffectLevelValue.value;
 
+  window.initializeScale(uploadFileElement, scaleElement, setScale);
+
+  window.initializeFilters.effect(uploadEffectControlElement, setFilter, 100);
+
   uploadEffectLevelElement.classList.add(OVERLAY_HIDDEN_CLASS);
 
   formElement.action = 'https://js.dump.academy/kekstagram';
-  uploadResizeControlsValue.step = SCALE_STEP;
 
   uploadFileElement.addEventListener('change', onInputFileChange);
   uploadFormCancelElement.addEventListener('click', onCancelButtonClick);
   uploadFormDescriptionElement.maxLength = MAX_COMMENT_LENGTH;
   uploadFormDescriptionElement.addEventListener('focus', onCommentInputOnFocus);
   uploadFormDescriptionElement.addEventListener('blur', onCommentInputBlur);
-  uploadEffectControlElement.addEventListener('change', onRadioControlEffectChange);
-  uploadResizeControlsButtonDec.addEventListener('click', onResizeControlsButtonDecClick);
-  uploadResizeControlsButtonInc.addEventListener('click', onResizeControlsButtonInkClick);
+
   uploadFormDescriptionElement.addEventListener('change', onChangeCommentElement);
   uploadFormHashtagsElement.addEventListener('change', onChangeTagsElement);
   document.addEventListener('keydown', onDocumentKeydown);
