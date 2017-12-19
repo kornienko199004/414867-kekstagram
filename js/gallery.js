da0ccbeda8d1d45f4a46c1671b278d3d4bf1412c
kornienko199004 <kornienko19904@gmail.com>
2017-12-19 22:43:03 +0300

    Добавил сортировку фотографий

--- js/gallery.js
+++ js/gallery.js
@@ -32,6 +32,9 @@
       likes: ['.picture-likes', 'textContent']
     });
     picturesContainerElement.appendChild(pictureList);
+    if (filtersElement.classList.contains('filters-inactive')) {
+      filtersElement.classList.remove('filters-inactive');
+    }
   };
   var onLoad = function (data) {
     pictures = data.map(function (item) {
@@ -42,6 +45,67 @@
     renderPictures(pictures);
   };
 
+  var generateRandomNumber = function (startNumber, endNumber) {
+    return Math.round(Math.random() * (endNumber - startNumber)) + startNumber;
+  };
+
+  (function () {
+    var DEBOUNCE_INTERVAL = 500;
+
+    var lastTimeout;
+    window.debounce = function (fun) {
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(fun, DEBOUNCE_INTERVAL);
+    };
+  })();
+
+  var returnSortingData = function (picturesArray, picturesArrayCopy, typeOfSorting) {
+    switch (typeOfSorting) {
+      case 'filter-recommend':
+        return picturesArray;
+      case 'filter-popular':
+        return picturesArrayCopy.sort(function (first, second) {
+          return second.likes - first.likes;
+        });
+      case 'filter-discussed':
+        return picturesArrayCopy.sort(function (first, second) {
+          return second.commentsCount - first.commentsCount;
+        });
+      case 'filter-random':
+        return (function () {
+          var element;
+          var number;
+          var newPicturesArray = [];
+          while (picturesArrayCopy.length > 0) {
+            number = generateRandomNumber(0, picturesArrayCopy.length - 1);
+            element = picturesArrayCopy[number];
+            newPicturesArray.push(element);
+            picturesArrayCopy.splice(number, 1);
+          }
+          return newPicturesArray;
+        })();
+      default:
+        break;
+    }
+    return 'none';
+  };
+
+  var onSortingMethodChange = function (e) {
+    var sortingType = e.target.id;
+    var children = picturesContainerElement.querySelectorAll('.picture');
+    for (var i = children.length - 1; i >= 0; i--) {
+      var child = children[i];
+      picturesContainerElement.removeChild(child);
+    }
+
+    window.debounce(function () {
+      renderPictures(returnSortingData(pictures, pictures.slice(0), sortingType));
+    });
+
+  };
+
   var pictureList;
 
   var pictures;
@@ -50,7 +114,7 @@
   var galleryOverlayElement = document.querySelector('.gallery-overlay');
   var galleryOverlayElementCloseElement = document.querySelector('.gallery-overlay-close');
   var lastPictureFocused;
-
+  var filtersElement = document.querySelector('.filters');
   galleryOverlayElementCloseElement.tabIndex = 0;
 
   window.backend.load(onLoad, window.errorPopup.show);
@@ -65,4 +129,5 @@
   galleryOverlayElementCloseElement.addEventListener('keydown', onCloseButtonKeydown);
   document.addEventListener('keydown', onDocumentKeydown);
 
+  filtersElement.addEventListener('change', onSortingMethodChange);
 })();
