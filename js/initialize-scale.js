ad54940c8485d3eac458d52772b09783ee054c73
kornienko199004 <kornienko19904@gmail.com>
2017-12-21 23:51:37 +0300

    uploadFileElement -> fileElement, uploadFileElementLabel -> dropElement

--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -6,7 +6,7 @@
   var SCALE_STEP = 25;
   var DEFAULT_SCALE = 55;
 
-  window.initializeScale = function (uploadFileElement, uploadFileElementLabel, scaleElement, adjustScale) {
+  window.initializeScale = function (fileElement, dropElement, scaleElement, adjustScale) {
     var onResizeControlsButtonDecClick = function () {
       scale = +uploadResizeControlsValue.value.slice(0, uploadResizeControlsValue.value.length - 1) - SCALE_STEP;
       if (scale < MIN_SCALE) {
@@ -37,7 +37,7 @@
 
     uploadResizeControlsButtonInc.addEventListener('click', onResizeControlsButtonInkClick);
     uploadResizeControlsButtonDec.addEventListener('click', onResizeControlsButtonDecClick);
-    uploadFileElement.addEventListener('change', setDefaultScale);
-    uploadFileElementLabel.addEventListener('drop', setDefaultScale);
+    fileElement.addEventListener('change', setDefaultScale);
+    dropElement.addEventListener('drop', setDefaultScale);
   };
 })();
